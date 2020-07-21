3eba471a792162da234771d69c7704df1487c12d
ta264 <ta264@users.noreply.github.com>
2020-07-21 21:16:57 +0100

    TO REVERT: work around net 5 regex bug until fix in preview 8
    https://github.com/dotnet/runtime/issues/39390

--- src/NzbDrone.Common/Instrumentation/CleanseLogMessage.cs
+++ src/NzbDrone.Common/Instrumentation/CleanseLogMessage.cs
@@ -43,7 +43,7 @@ namespace NzbDrone.Common.Instrumentation
                 new Regex(@"(?<=\?|&)(authkey|torrent_pass)=(?<secret>[^&=]+?)(?=""|&|$)", RegexOptions.Compiled | RegexOptions.IgnoreCase),
 
                 // Plex
-                new Regex(@"(?<=\?|&)(X-Plex-Client-Identifier|X-Plex-Token)=(?<secret>[^&=]+?)(?= |&|$)", RegexOptions.Compiled | RegexOptions.IgnoreCase)
+                new Regex(@"(?<=\?|&)(X-Plex-Client-Identifier|X-Plex-Token)=(?<secret>[^&=]+?)(?= |&|$)", RegexOptions.IgnoreCase)
             };
 
         private static readonly Regex CleanseRemoteIPRegex = new Regex(@"(?:Auth-\w+(?<!Failure|Unauthorized) ip|from) (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", RegexOptions.Compiled);
